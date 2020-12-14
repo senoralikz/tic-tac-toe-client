e8e22d11e11242a28ec809a21adc2f4fe4b4aaf6
senoralikz <alexjm825@gmail.com>
2020-12-14 00:53:42 -0500

    Added curl script to index games

---
+++ curl-scripts/index-games.sh
@@ -0,0 +1,10 @@
+curl "https://tic-tac-toe-api-production.herokuapp.com/games" \
+  --include \
+  --request GET \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "games": []
+    }'
+
+echo
